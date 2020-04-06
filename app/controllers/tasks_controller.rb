ccc0aa00aee192b75fd7b5619ea449e04a81cfa3
akihiro.takano <suiren951@gmail.com>
2020-04-06 07:49:40 +0000

    heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,11 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:edit, :update, :destroy, :show]
   
   def index
     @tasks = Task.all
-  end
   
   def show
-    set_task
   end
   
   def new
@@ -14,7 +13,7 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'Task が正常に作成されました'
@@ -26,11 +25,9 @@ class TasksController < ApplicationController
   end
   
   def edit
-    set_task
   end
   
   def update
-    set_task
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
@@ -41,7 +38,6 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    set_task
     @task.destroy
     
     flash[:success] = 'Task は正常に削除されました'
@@ -50,11 +46,14 @@ class TasksController < ApplicationController
   
   private
   
-  def set_task
-    @task = Task.find(params[:id])
-  end
-  
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
