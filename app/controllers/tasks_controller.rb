b59d797811e6a92c9b06e28c1cc87be2a637f3e9
akihiro.takano <suiren951@gmail.com>
2020-04-06 09:51:18 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,8 @@ class TasksController < ApplicationController
   before_action :correct_user, only: [:edit, :update, :destroy, :show]
   
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks
+  end
   
   def show
   end
